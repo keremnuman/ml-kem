1467d6ad7ecc1e96115c97e098aa75c206985d51
Kerem Numan Çakır <cakirkeremnuman@gmail.com>
2026-04-02 16:36:37 +0300

    header files design

--- algorithms.c
+++ algorithms.c
@@ -2,3 +2,14 @@
 #include <stdlib.h>
 
 #include "algorithms.h"
+
+void bits_to_bytes(const uint8_t *b, size_t bit_len, uint8_t *B)
+{
+    size_t byte_len = bit_len / 8;
+    memset(B, 0, byte_len);
+
+    for (size_t i = 0; i < bit_len; i++) {
+        /* i. bit → B[i/8] içindeki (i%8). pozisyon */
+        B[i / 8] |= (uint8_t)(b[i] << (i % 8));
+    }
+}
--- algorithms.h
+++ algorithms.h
@@ -1,7 +1,25 @@
 #ifndef ALGORITHMS_H
 #define ALGORITHMS_H
+#include <stdint.h>
 
+#define K 2
+#define N 256
+#define Q 3329
 
+// 256 için 16 bit aldım.
+typedef struct {
+    int16_t coefficients[N];
+} polynom;
+
+typedef struct {
+    polynom vector[K];
+} polynom_vector;
+
+typedef struct {
+    polynom matrix[K][K];
+} polynom_matrix;
+
+void bits_to_bytes(const uint8_t *b, size_t bit_len, uint8_t *B);
 
 
 
