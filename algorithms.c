9f204a17a66a60e609f55f569ca9a1a653661499
Kerem Numan Çakır <cakirkeremnuman@gmail.com>
2026-04-03 14:59:58 +0300

    stup 2

--- algorithms.c
+++ algorithms.c
@@ -9,7 +9,19 @@ void bits_to_bytes(const uint8_t *b, size_t bit_len, uint8_t *B)
     memset(B, 0, byte_len);
 
     for (size_t i = 0; i < bit_len; i++) {
-        /* i. bit → B[i/8] içindeki (i%8). pozisyon */
-        B[i / 8] |= (uint8_t)(b[i] << (i % 8));
+        // i. bit → B[i/8] içindeki (i%8). pozisyon
+        B[i / 8] |= (uint8_t)(b[i] << (i % 8)); // En son toplarım.
     }
+}
+
+void bytes_to_bits(const uint8_t *B, size_t byte_len, uint8_t *b){
+    size_t bit_len = byte_len * 8;
+    memset(b, 0, bit_len);
+    for (size_t i = 0; i < byte_len; i++) {
+        // i. bit → B[i/8] içindeki (i%8). pozisyon
+        uint8_t byte = B[i];
+        for (size_t j = 0; j < 8; j++) {
+            b[i * 8 + j] = (byte >> j);
+        }
+    }       
 }
--- algorithms.h
+++ algorithms.h
@@ -6,9 +6,9 @@
 #define N 256
 #define Q 3329
 
-// 256 için 16 bit aldım.
+// 256 için 8 bit aldım.
 typedef struct {
-    int16_t coefficients[N];
+    int8_t coefficients[N];
 } polynom;
 
 typedef struct {
@@ -20,6 +20,8 @@ typedef struct {
 } polynom_matrix;
 
 void bits_to_bytes(const uint8_t *b, size_t bit_len, uint8_t *B);
+
+void bytes_to_bits(const uint8_t *B, size_t byte_len, uint8_t *b);
 
 
 
